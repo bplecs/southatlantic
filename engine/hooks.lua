f103960cef0b38ba4ade50930dfe040c678cd0ef
Gordy <gordy@inter.net>
2025-05-23 15:14:01 -0400

    initial dll and hooks.lua

---
+++ engine/hooks.lua
@@ -0,0 +1,14 @@
+
+package.cpath = package.cpath .. ";" .. lfs.writedir() .. "\\Scripts\\?.dll"
+
+net.log("loading gordyengine")
+-- Trigger these functions to fire when these events occur in DCS
+local gordyengine = require("gordyengine")
+DCS.setUserCallBacks( { 
+onPlayerTryConnect = gordyengine.on_player_try_connect()
+--onSimulationFrame = 
+onPlayerDisconnect = gordyengine.on_player_disconnect()
+--onPlayerTrySendChat = 
+--onMissionLoadEnd = 
+--onPlayerTryChangeSlot = 
+})
